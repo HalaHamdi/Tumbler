7e1509787dbca65ec0cc39421badfa2229288ba2
Halahamdy22 <halahamdyabdelhady@gmail.com>
2021-11-11 21:24:52 +0200

    Uploading Navbar

--- app/src/main/java/com/example/tumbler/MainActivity.kt
+++ app/src/main/java/com/example/tumbler/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.tumbler
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.fragment)
+        val btnNavView = findViewById<BottomNavigationView>(R.id.footer_navigation)
+        btnNavView.setupWithNavController(navController)
     }
 }
